4e7a5db1177f8c3eadb966f4714d0667a07335e5
GabrielSa <gabrielsa95.2@gmail.com>
2024-04-03 14:59:21 -0300

    improve

--- src/models/repository/establishment_type_repository.py
+++ src/models/repository/establishment_type_repository.py
@@ -1,18 +1,23 @@
 from typing import Dict
 from src.models.settings.connection import connection_handler
 from src.models.entities.establishment_types import EstablishmentTypes
-
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.orm.exc import UnmappedInstanceError
 class EstablishmentTypesRepository():
     def insert(self, establishment_type: Dict ) ->  Dict:
         with connection_handler as database: 
-            establishment_type = EstablishmentTypes(
-                id=establishment_type.get("uuid"),
-                name=establishment_type.get("name")
-            )
-            database.session.add(establishment_type)
-            database.session.commit()
+            try:
+                establishment_type = EstablishmentTypes(
+                    id=establishment_type.get("uuid"),
+                    name=establishment_type.get("name")
+                )
+                database.session.add(establishment_type)
+                database.session.commit()
 
-            return establishment_type
+                return establishment_type
+            except IntegrityError:
+                database.session.rollback()
+                raise Exception("Establishment already exists or not found id_type.")
 
     def find_by_id(self, establishment_type_id: Dict) -> Dict:
         with connection_handler as database:
@@ -21,7 +26,11 @@ class EstablishmentTypesRepository():
         
     def delete(self, establishment_type_id: Dict) -> Dict:
         with connection_handler as database:
-            establishment_type = database.session.query(EstablishmentTypes).filter_by(id=establishment_type_id).first()
-            database.session.delete(establishment_type)
-            database.session.commit()
-            return establishment_type
+            try:
+                establishment_type = database.session.query(EstablishmentTypes).filter_by(id=establishment_type_id).first()
+                database.session.delete(establishment_type)
+                database.session.commit()
+                return establishment_type
+            except UnmappedInstanceError:
+                database.session.rollback()
+                raise Exception("Could not delete establishment while is not found.")
